b6e62b8aee5e75dc1031236dc9275963d084074a
Mikhail Andreev <mandreev@iponweb.com>
2020-06-04 21:05:06 +0200

    change filter

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,36 @@
 use serde::{Serialize, Deserialize};
-use bloom::{ASMS,BloomFilter};
+use bloomfilter::Bloom;
+use siphasher::sip::SipHasher;
+use std::hash::BuildHasher;
 
 
 #[derive(Serialize, Deserialize, Debug)]
-struct Point {
-    x: i32,
-    y: i32,
+struct InternalState {
+    x: u64,
+    y: u64,
 }
+
+impl BuildHasher for InternalState {
+    type Hasher = SipHasher;
+
+    fn build_hasher(&self) -> SipHasher {
+        SipHasher::new_with_keys(self.x,self.y)
+    }
+}
+
 fn main() {
     // The object that we will serialize.
-    let target: Option<String>  = Some("hello world".to_string());
-
-    let encoded: Vec<u8> = bincode::serialize(&target).unwrap();
-    let decoded: Option<String> = bincode::deserialize(&encoded[..]).unwrap();
-    assert_eq!(target, decoded);
 
     let expected_num_items = 1000;
 
     // out of 100 items that are not inserted, expect 1 to return true for contain
     let false_positive_rate = 0.01;
 
-    let mut filter = BloomFilter::with_rate(false_positive_rate,expected_num_items);
-    filter.insert(&1);
-    println!("{}",filter.contains(&1)); /* true */
-    println!("{}",filter.contains(&2)); /* probably false */
-    let encoded2: Vec<u8> = bincode::serialize(&filter).unwrap();
+
+    let mut filter = Bloom::new_for_fp_rate(expected_num_items, false_positive_rate);
+    filter.set(&1);
+    println!("{}", filter.check(&1)); /* true */
+    println!("{}", filter.check(&2)); /* probably false */
+    println!("{}", serde_json::to_string(&SipHasher::new_with_keys(10,10)).unwrap());
+    
 }
