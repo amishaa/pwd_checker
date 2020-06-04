a564aadf3d8dcd2017d41da765f7e682b8ea257e
Mikhail Andreev <mandreev@iponweb.com>
2020-06-05 01:37:15 +0200

    work with pwds

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,48 @@
 use serde::{Serialize, Deserialize};
-use siphasher::sip::SipHasher;
-use std::hash::BuildHasher;
-
+use serde_json;
+use std::{fs,io::{self, BufRead}};
 
 mod bloom;
 use bloom::Bloom;
 
-#[derive(Serialize, Deserialize, Debug)]
-struct InternalState {
-    x: u64,
-    y: u64,
-}
-
-impl BuildHasher for InternalState {
-    type Hasher = SipHasher;
-
-    fn build_hasher(&self) -> SipHasher {
-        SipHasher::new_with_keys(self.x,self.y)
-    }
-}
-
 fn main() {
     // The object that we will serialize.
 
-    let expected_num_items = 1000;
+    let expected_num_items = 600_000_000;
 
     // out of 100 items that are not inserted, expect 1 to return true for contain
-    let false_positive_rate = 0.01;
+    let false_positive_rate = 0.07;
 
 
     let mut filter = Bloom::new_for_fp_rate(expected_num_items, false_positive_rate);
     filter.set(&1);
-    println!("{}", filter.check(&1)); /* true */
-    println!("{}", filter.check(&2)); /* probably false */
-    println!("{}", serde_json::to_string(&filter).unwrap());
+
+    fill_filter_with_pwd ("Test", & mut filter).unwrap();
+//    println!("{}", serde_json::to_string(&filter).unwrap());
     
+}
+
+fn fill_filter_with_pwd<'a> (filename: &str,  filter: &'a mut Bloom) -> io::Result<&'a Bloom>
+{
+    let contents = fs::File::open(filename)?;
+    let buf_reader = io::BufReader::new(contents);
+
+    Ok(fill_filter_with_strings(buf_reader.lines().map(|line| normalize_string(&line.unwrap())), filter))
+
+}
+
+fn normalize_string (s:&str) -> String
+{
+    s.to_lowercase()
+}
+
+
+fn fill_filter_with_strings<'a, I> (strings: I, filter: &'a mut Bloom) -> &'a Bloom
+where 
+    I: Iterator<Item=String>
+{
+    for string in strings {
+        filter.set(&string);
+    }
+    filter
 }
